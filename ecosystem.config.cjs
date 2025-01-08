7c2894fca3b7b5e54226078590cdcd1022f503ff
Krowemoh <krowemoh@hotmail.com>
2025-01-08 00:20:14 -0500

    Added better logging for pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
     apps : [{
         name   : "StarterApplication",
-        script : "ORIGIN=https://example.org BODY_SIZE_LIMIT=Infinity node build/index.js"
+        script : "ORIGIN=https://example.org BODY_SIZE_LIMIT=Infinity node build/index.js",
+        log_date_format: "YYYY-MM-DD HH:mm Z",
     }]
 }
